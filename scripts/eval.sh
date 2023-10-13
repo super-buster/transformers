ddd97a9d3a17e1421717ae6d6d7bc8dae1d4cbdb
yanzhongxiang <424295833@qq.com>
2023-10-13 17:15:05 +0800

    add evaluate & predict script

---
+++ scripts/eval.sh
@@ -0,0 +1,21 @@
+random_string=hdDJr
+echo "随机字符串: $random_string"
+CUDA_VISIBLE_DEVICES="7" python -u /share/yanzhongxiang/transformers/examples/pytorch/text-classification/run_glue.py \
+--model_name_or_path /share/yanzhongxiang/models/bert-1012-2merge-hdDJr/checkpoint-2940 \
+--task_name newsflash/twitter \
+--train_file /share/yanzhongxiang/newsflash_classification/data/2merge_train.jsonl \
+--validation_file /share/yanzhongxiang/newsflash_classification/data/aih_title_val.jsonl \
+--test_file /share/yanzhongxiang/newsflash_classification/data/all_aih_title_content_test.jsonl \
+--output_dir /share/yanzhongxiang/models/bert-1012-2merge-$random_string \
+--fp16 \
+--overwrite_cache \
+--do_eval \
+--max_seq_length 512 \
+--per_device_train_batch_size 128  \
+--per_device_eval_batch_size 128 \
+--learning_rate 2e-5 \
+--num_train_epochs 3 \
+--seed 42  \
+--save_strategy epoch \
+--evaluation_strategy epoch \
+--save_total_limit 3 \
